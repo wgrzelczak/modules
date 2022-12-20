556f3e2f1f090a84fcd0d558611327f49627b2f6
wgrzelczak <wojciech.grzelczak@gmail.com>
2022-12-20 00:39:52 +0000

    ImGuizmo

---
+++ ImGui_ImGuizmo/source/imgui.h
@@ -0,0 +1,4 @@
+// dummy header with redirection to imgui module
+#include "../../ImGui/source/imgui.h"
+
+#pragma comment(lib, "../ImGui/windows/ImGui")
---
+++ ImGui_ImGuizmo/source/imgui_internal.h
@@ -0,0 +1,2 @@
+// dummy header with redirection to imgui module
+#include "../../ImGui/source/imgui_internal.h"
